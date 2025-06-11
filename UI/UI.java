0c816acd0785efe3a20dafd6f8f86db487e1fa05
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2025-06-11 17:51:24 -0300

    feat: added ui responsaveis type and valid format

--- UI/UI.java
+++ UI/UI.java
@@ -6,42 +6,41 @@ import Prefeitura.Prefeitura;
 
 public class UI {
     public static void startUI() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int choice = 0;
+        Scanner scanner = new Scanner(System.in);
+        int choice = 0;
+        
+        System.out.println("\n--- Bem vindo ao painel do IPTU ---");
+        
+        Prefeitura prefeitura = new Prefeitura(60, 75, 0.15, 0.2);
+        
+        while (choice != 4) {
+            clearScreen();
+            System.out.println("1. Responsáveis");
+            System.out.println("2. Imóveis");
+            System.out.println("3. Mostrar IPTUs");
+            System.out.println("4. Exit");
+            System.out.print("\nEnter your choice: ");
+            choice = scanner.nextInt();
             
-            System.out.println("\n--- Bem vindo ao painel do IPTU ---");
-            
-            Prefeitura prefeitura = new Prefeitura(60, 75, 0.15, 0.2);
-            
-            while (choice != 4) {
-                clearScreen();
-                System.out.println("1. Responsáveis");
-                System.out.println("2. Imóveis");
-                System.out.println("3. Mostrar IPTUs");
-                System.out.println("4. Exit");
-                System.out.print("\nEnter your choice: ");
-                choice = scanner.nextInt();
-                
-                switch (choice) {
-                    case 1 -> {
-                        clearScreen();
-                        Responsaveis.startMenu(scanner, prefeitura);
-                    }
-                    case 2 -> {
-                        clearScreen();
-                        Imoveis.startMenu(scanner, prefeitura);
-                    }
-                    case 3 -> {
-                        clearScreen();
-                        PrefeituraUI.startMenu(scanner, prefeitura);
-                    }
-                    case 4 -> {
-                        clearScreen();
-                        System.out.println("Exiting...");
-                        return;
-                    }
-                    default -> throw new AssertionError();
+            switch (choice) {
+                case 1 -> {
+                    clearScreen();
+                    Responsaveis.startMenu(scanner, prefeitura);
                 }
+                case 2 -> {
+                    clearScreen();
+                    Imoveis.startMenu(scanner, prefeitura);
+                }
+                case 3 -> {
+                    clearScreen();
+                    PrefeituraUI.startMenu(scanner, prefeitura);
+                }
+                case 4 -> {
+                    clearScreen();
+                    System.out.println("Exiting...");
+                    return;
+                }
+                default -> throw new AssertionError();
             }
         }
     }
